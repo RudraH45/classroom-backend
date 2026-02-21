6d27b5666edbef2f16960988075b417ca8ef0cb8
RudraH45 <rudra89howale@gmail.com>
2026-02-21 19:18:18 +0530

    implemented cors

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,21 @@ import { eq } from 'drizzle-orm';
 import { db } from './db';
 // import { demoUsers } from './db/schema';
 import subjectRoutes from './routes/subjects';
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
+
+if(!process.env.FRONTEND_URL){
+  throw new Error('FRONTEND_URL is not defined in .env file');
+} 
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    }
+))
 
 app.use(express.json());
 app.use('/api/subjects', subjectRoutes);
